4352adcd416b459b2a3c98b25cbc7ddd4ced7a05
Zongwei Huang <41694509+Resuscit@users.noreply.github.com>
2023-03-29 16:30:25 -0400

    Add files via upload
    Add a correlation function checking function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 //       2022.12.6         //
 /////////////////////////////
 // This is a population-only non-condon version for the driven system QME
+// version 1.1 update: checked with yifan and pouya's result, known error fixed
+// version 1.2 update: add correlation function check
 
 #include <iostream>
 #include "constant.h"
@@ -19,6 +21,7 @@ int main(){
     input(Hami_sim,time_sim,stat_sim);
     error_flag = initialize(Hami_sim,time_sim,stat_sim);
     if(error_flag == 1){return 0;}
+    check_correlation_function(Hami_sim);
     evolution(Hami_sim,time_sim,stat_sim);
     //analysis();
     //output();
