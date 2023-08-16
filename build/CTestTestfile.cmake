62fb5ed8b8cf805ac062004c216fdf26930c342b
Zongwei Huang <41694509+Resuscit@users.noreply.github.com>
2023-08-16 15:24:42 -0400

    Delete CTestTestfile.cmake

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/huangzongwei/Documents/Academic/Umich/2022/Geva/driven_QME/cpp/4.0
-# Build directory: /Users/huangzongwei/Documents/Academic/Umich/2022/Geva/driven_QME/cpp/4.0/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(driven_QME "/Users/huangzongwei/Documents/Academic/Umich/2022/Geva/driven_QME/cpp/4.0/driven_QME")
-set_tests_properties(driven_QME PROPERTIES  _BACKTRACE_TRIPLES "/Users/huangzongwei/Documents/Academic/Umich/2022/Geva/driven_QME/cpp/4.0/CMakeLists.txt;21;add_test;/Users/huangzongwei/Documents/Academic/Umich/2022/Geva/driven_QME/cpp/4.0/CMakeLists.txt;0;")
